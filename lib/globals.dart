ad477df6259a34d3a6e535506032252695541816
duccdev <usr.bin.ducc.2@gmail.com>
2024-06-26 14:35:15 +0300

    chore: clean deps, feat: add proper connection test

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,8 +1,9 @@
+import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class Globals {
-  static String? model;
   static String? apiKey;
+  static GenerativeModel? model;
   static late SharedPreferences prefs;
 
   static Future init() async {
@@ -11,7 +12,19 @@ class Globals {
   }
 
   static void reload() {
-    model = prefs.getString('model');
     apiKey = prefs.getString('apiKey');
+
+    if (apiKey != null) {
+      model = GenerativeModel(
+        model: 'gemini-1.5-flash',
+        apiKey: apiKey!,
+        safetySettings: [
+          SafetySetting(HarmCategory.dangerousContent, HarmBlockThreshold.none),
+          SafetySetting(HarmCategory.harassment, HarmBlockThreshold.none),
+          SafetySetting(HarmCategory.hateSpeech, HarmBlockThreshold.none),
+          SafetySetting(HarmCategory.sexuallyExplicit, HarmBlockThreshold.none),
+        ],
+      );
+    }
   }
 }
--- lib/pages/setup.dart
+++ lib/pages/setup.dart
@@ -4,6 +4,7 @@ import 'package:gemmy/utils/pad_widgets.dart';
 import 'package:gemmy/widgets/ez/button.dart';
 import 'package:gemmy/widgets/ez/text.dart';
 import 'package:gemmy/widgets/ez/textfield.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
 
 class SetupPage extends StatefulWidget {
   const SetupPage({super.key});
@@ -23,60 +24,80 @@ class _SetupPageState extends State<SetupPage> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: padWidgets(
-          const EdgeInsets.only(bottom: 16),
+          const EdgeInsets.only(bottom: 8),
           <Widget>[
-            const EzText('Step 1: Add your API key'),
             SizedBox(
               width: 512,
               child: EzTextField(
                 enabled: _step1Enabled,
-                hintText: 'I\'m not looking!',
+                hintText: 'Put your API key here... I\'m not looking!',
                 obscureText: true,
                 disableSubmitButton: true,
                 onChanged: (text) {
                   text = text.trim();
 
                   if (text.isEmpty) {
-                    Globals.apiKey = null;
                     Globals.prefs.remove('apiKey');
+                    Globals.reload();
                     setState(() => _step2Enabled = false);
                     return;
                   }
 
-                  Globals.apiKey = text;
                   Globals.prefs.setString('apiKey', text);
+                  Globals.reload();
                   setState(() => _step2Enabled = true);
                 },
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const EzText('Step 2: '),
-                SizedBox(
-                  width: 150,
-                  child: EzButton(
-                    onPressed: _step2Enabled
-                        ? () => setState(() {
-                              _step1Enabled = false;
-                              _step2Enabled = false;
-                              _step2Loading = true;
-                            })
-                        : null,
-                    child: _step2Loading
-                        ? const SizedBox(
-                            height: 24,
-                            width: 24,
-                            child: CircularProgressIndicator(),
-                          )
-                        : EzText(
-                            'Connect',
-                            color:
-                                _step2Enabled ? Colors.white : Colors.white54,
-                          ),
-                  ),
-                ),
-              ],
+            SizedBox(
+              width: 150,
+              height: 42,
+              child: EzButton(
+                onPressed: _step2Enabled
+                    ? () async {
+                        setState(() {
+                          _step1Enabled = false;
+                          _step2Enabled = false;
+                          _step2Loading = true;
+                        });
+
+                        try {
+                          await Globals.model!
+                              .generateContent([Content.text('Hi!')]);
+
+                          // ignore: use_build_context_synchronously
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: EzText('Connected!'),
+                            ),
+                          );
+                        } catch (_) {
+                          // ignore: use_build_context_synchronously
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                              content: EzText('Failed!'),
+                            ),
+                          );
+
+                          setState(() {
+                            _step1Enabled = true;
+                            _step2Enabled = true;
+                            _step2Loading = false;
+                          });
+                        }
+                      }
+                    : null,
+                child: _step2Loading
+                    ? const SizedBox(
+                        height: 24,
+                        width: 24,
+                        child: CircularProgressIndicator(),
+                      )
+                    : EzText(
+                        'Connect',
+                        color: _step2Enabled ? Colors.white : Colors.white54,
+                      ),
+              ),
             ),
           ],
         ),
